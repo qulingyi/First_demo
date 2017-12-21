e8c41a161aa8954e3df0ac3cec7552aa87039fc4
Lingyi Qu <lingyiququ@gmail.com>
2017-12-20 17:41:47 -0800

    move+rotate

--- Demo_1.0/Assets/CameraMovement.cs
+++ Demo_1.0/Assets/CameraMovement.cs
@@ -1,16 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class CameraMovement : MonoBehaviour {
+public class CameraMovement : MonoBehaviour
+{
+    //
+    // VARIABLES
+    //
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+    public float turnSpeed = 4.0f;      // Speed of camera turning when mouse moves in along an axis
+    private Vector3 mouseOrigin;    // Position of cursor when mouse dragging starts
+    private bool isRotating;    // Is the camera being rotated
+    private bool isMoving;
+    public float speed = 10.0f;
+
+    void Start()
+    {
+
+        mouseOrigin = transform.position;
+    }
+
+    void Update()
+    {
+        // Get the left mouse button
+        if (Input.GetMouseButtonDown(1))
+        {
+            // Get mouse origin
+            mouseOrigin = Input.mousePosition;
+            isRotating = true;
+        }
+        // Disable movements on button release
+        if (!Input.GetMouseButton(1))
+        {
+            isRotating = false;
+        }
+        // Rotate camera along X and Y axis
+        if (isRotating)
+        {
+            Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - mouseOrigin);
+
+            transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
+            transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            isMoving = true;
+        }
+        // Disable movements on button release
+        if (!Input.GetMouseButton(0))
+        {
+            isMoving = false;
+        }
+
+        if (isMoving)
+        {
+            if (Input.GetAxis("Mouse X") > 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                                           0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+            }
+
+            else if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.position += new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                                           0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+            }
+        }
+      
+    }
+
+ }
