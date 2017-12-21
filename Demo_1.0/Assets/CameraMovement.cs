94ec4ce355c61e1db66dff6c02b033c43dc47ced
Lingyi Qu <lingyiququ@gmail.com>
2017-12-21 12:37:22 -0800

    Panorama cubemap

--- Demo_1.0/Assets/CameraMovement.cs
+++ Demo_1.0/Assets/CameraMovement.cs
@@ -3,19 +3,14 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
-    //
-    // VARIABLES
-    //
-
     public float turnSpeed = 4.0f;      // Speed of camera turning when mouse moves in along an axis
     private Vector3 mouseOrigin;    // Position of cursor when mouse dragging starts
     private bool isRotating;    // Is the camera being rotated
-    private bool isMoving;
-    public float speed = 10.0f;
+    private bool isMoving;     // Is the camera being moved
+    public float speed = 10.0f; //Speed of camera moving when mouse left click holding 
 
     void Start()
-    {
-
+    { 
         mouseOrigin = transform.position;
     }
 
@@ -41,7 +36,7 @@ public class CameraMovement : MonoBehaviour
             transform.RotateAround(transform.position, transform.right, -pos.y * turnSpeed);
             transform.RotateAround(transform.position, Vector3.up, pos.x * turnSpeed);
         }
-
+        //Get the left side button
         if (Input.GetMouseButtonDown(0))
         {
             isMoving = true;
@@ -51,7 +46,7 @@ public class CameraMovement : MonoBehaviour
         {
             isMoving = false;
         }
-
+        //Move along X and Y axis
         if (isMoving)
         {
             if (Input.GetAxis("Mouse X") > 0)
@@ -66,7 +61,18 @@ public class CameraMovement : MonoBehaviour
                                            0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
             }
         }
-      
+
+        if(Input.GetAxis("Mouse ScrollWheel")>0)
+        {
+            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y - .2f,
+                                            transform.position.z + .2f);
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            GetComponent<Transform>().position = new Vector3(transform.position.x, transform.position.y + .2f,
+                                            transform.position.z - 2f);
+        }
+
     }
 
  }
