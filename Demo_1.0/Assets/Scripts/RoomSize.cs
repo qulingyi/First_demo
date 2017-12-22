508eebb600c9968ed62f45410f9a273690391944
Lingyi Qu <lingyiququ@gmail.com>
2017-12-21 18:57:57 -0800

    new background, scaling in process

---
+++ Demo_1.0/Assets/Scripts/RoomSize.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class RoomSize : MonoBehaviour {
+    public GameObject room;
+    public float scale;
+    private Vector3 roomOriginal;
+	// Use this for initialization
+	void Start () {
+        roomOriginal = room.transform.position;
+        Debug.Log(room.transform.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0))
+        {
+            room.transform.localScale *= scale/100.0f;
+            room.transform.position = roomOriginal;
+            Debug.Log(room.transform.position);
+        }
+	}
+}
