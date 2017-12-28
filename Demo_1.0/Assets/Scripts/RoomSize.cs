9d8508a31287ed0dd4068b67068b0eab6d2bfa90
Lingyi Qu <lingyiququ@gmail.com>
2017-12-27 17:53:32 -0800

    room scale fixed

--- Demo_1.0/Assets/Scripts/RoomSize.cs
+++ Demo_1.0/Assets/Scripts/RoomSize.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 public class RoomSize : MonoBehaviour {
     public GameObject room;
-    public float scale;
-    //private Vector3 roomOriginal;
+    //public float scale;
+    private Vector3 roomOriginal;
 	// Use this for initialization
 	void Start () {
-        //roomOriginal = room.transform.position;
+        roomOriginal = room.transform.position;
         //Debug.Log(room.transform.position);
+
 	}
 	
 	// Update is called once per frame
@@ -16,8 +17,10 @@ public class RoomSize : MonoBehaviour {
 		if(Input.GetMouseButtonDown(0))
         {
             //room.transform.localScale *= scale/100.0f;
-            //room.transform.position = roomOriginal;
+            
             //Debug.Log(room.transform.position);
+            room.transform.localScale += new Vector3(2, 2, 2);
+            room.transform.position = roomOriginal;
         }
-	}
+    }
 }
---
+++ Demo_1.0/Assets/Scripts/URL.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class URL : MonoBehaviour {
+    public GameObject s;
+    
+  
+   
+    // Use this for initialization
+    void Start ()
+    {
+        //string q = Application.absoluteURL;
+        string q = "size=5";
+            //int t = q.Length - 1;
+        string[] stringSeparators = new string[] { "size=" };
+        //string m = q.Split("size="[0])[1];
+        string[] m = q.Split(stringSeparators, StringSplitOptions.None);
+        int si = int.Parse(m[1])-1;
+           s.transform.localScale += new Vector3(si, si, si);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+ 
+
+}
